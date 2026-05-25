50ad8b8fee5ea56983e23c04e497b90516610285
ZahinFyz <zahin.fyz@gmail.com>
2026-05-25 05:03:57 +0100

    'case_first_use'

---
+++ Associate_data_engineer/case_first_use.sql
@@ -0,0 +1,12 @@
+SELECT
+
+        CASE
+                WHEN hometeam_id = 10189 THEN 'FC Schalke 04'
+                WHEN hometeam_id = 9823 THEN 'FC Bayern Munich'
+                ELSE 'Other'
+        END AS home_team,
+
+        COUNT (id) AS total_matches
+
+FROM matches_germany
+GROUP BY home_team;
